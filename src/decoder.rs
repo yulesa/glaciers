75e70933fcd185a1792730bf1d35cfc4ff5eb062
yulesa <yulesa@gmail.com>
2024-12-07 18:16:08 -0300

    improving code, adding params_keys_mapping and params_mapping to decoded_log column

--- src/decoder.rs
+++ src/decoder.rs
@@ -14,17 +14,27 @@ pub enum DecodeError {
 }
 
 #[derive(Debug, Serialize)]
-struct Param {
+struct EventParam  {
     name: String,
     index: u32,
     value_type: String,
     value: String,
 }
 
-struct StringDynSolValue(DynSolValue);
+struct ExtendedDecodedEvent {
+     /// The indexed values, in order.
+     pub indexed: Vec<DynSolValue>,
+     /// The un-indexed values, in order.
+     pub body: Vec<DynSolValue>,
+     pub params_keys_mapping: Vec<String>,
+     pub params_mapping: String
+}
 
-impl StringDynSolValue {
-    pub fn to_string_representation(&self) -> Option<String> {
+//Wrapper type around DynSolValue, to implement a conversion to String function.
+struct StringifiedValue(DynSolValue);
+
+impl StringifiedValue {
+    pub fn convert(&self) -> Option<String> {
         match &self.0 {
             DynSolValue::Bool(b) => Some(b.to_string()),
             DynSolValue::Int(i, _) => Some(i.to_string()),
@@ -32,26 +42,26 @@ impl StringDynSolValue {
             DynSolValue::FixedBytes(w, _) => Some(format!("0x{}", w.to_string())),
             DynSolValue::Address(a) => Some(a.to_string()),
             DynSolValue::Function(f) => Some(f.to_string()),
-            DynSolValue::Bytes(b) => Some(format!("0x{}", String::from_utf8(b.to_vec()).expect("Our bytes should be valid utf8"))),
+            DynSolValue::Bytes(b) => Some(format!("0x{}", String::from_utf8_lossy(b))),
             DynSolValue::String(s) => Some(s.clone()),
             DynSolValue::Array(arr) => Some(format!(
                 "[{}]", 
                 arr.iter()
-                    .filter_map(|v| StringDynSolValue::from(v.clone()).to_string_representation())
+                    .filter_map(|v| Self::from(v.clone()).convert())
                     .collect::<Vec<_>>()
                     .join(", ")
             )),
             DynSolValue::FixedArray(arr) => Some(format!(
                 "[{}]", 
                 arr.iter()
-                    .filter_map(|v|  StringDynSolValue::from(v.clone()).to_string_representation())
+                    .filter_map(|v| Self::from(v.clone()).convert())
                     .collect::<Vec<_>>()
                     .join(", ")
             )),
             DynSolValue::Tuple(tuple) => Some(format!(
                 "({})", 
                 tuple.iter()
-                    .filter_map(|v|  StringDynSolValue::from(v.clone()).to_string_representation())
+                    .filter_map(|v| Self::from(v.clone()).convert())
                     .collect::<Vec<_>>()
                     .join(", ")
             ))
@@ -59,10 +69,9 @@ impl StringDynSolValue {
     }
 }
 
-// Conversion methods
-impl From<DynSolValue> for StringDynSolValue {
+impl From<DynSolValue> for StringifiedValue {
     fn from(value: DynSolValue) -> Self {
-        StringDynSolValue(value)
+        StringifiedValue(value)
     }
 }
 
@@ -70,10 +79,7 @@ pub fn decode_logs(df: DataFrame) -> Result<DataFrame, DecodeError> {
     df.lazy()
         .with_columns([
             as_struct(vec![col("topic0"), col("topic1"), col("topic2"), col("topic3"), col("data"), col("full_signature")])
-                .apply(
-                    |s| decode_log_batch(s),
-                    GetOutput::from_type(DataType::String),
-                )
+                .map(decode_log_batch, GetOutput::from_type(DataType::String))
                 .alias("decoded_log"),
         ])
         .collect()
@@ -81,25 +87,43 @@ pub fn decode_logs(df: DataFrame) -> Result<DataFrame, DecodeError> {
 }
 
 fn decode_log_batch(s: Series) -> PolarsResult<Option<Series>> {
-    let ca = s.struct_()?;
-    let fields = ca.fields();
+    let series_struct_array: &StructChunked = s.struct_()?;
+    let fields = series_struct_array.fields();
     
-    let ca_topic0 = fields[0].binary()?;
-    let ca_topic1 = fields[1].binary()?;
-    let ca_topic2 = fields[2].binary()?;
-    let ca_topic3 = fields[3].binary()?;
-    let ca_data = fields[4].binary()?;
-    let ca_sig = fields[5].str()?;
+    let topics_data = extract_log_fields(fields)?;
 
-    let out: StringChunked = ca_topic0
+    let out: StringChunked = topics_data
         .into_iter()
-        .zip(ca_topic1.into_iter())
-        .zip(ca_topic2.into_iter())
-        .zip(ca_topic3.into_iter())
-        .zip(ca_data.into_iter())
-        .zip(ca_sig.into_iter())
+        .map(|(topics, data, sig)| {
+            decode(sig, topics, data)
+                .map(|decoded_log| format!("{:?}, {:?}, {:?}, {}", decoded_log.indexed, decoded_log.body, decoded_log.params_keys_mapping, decoded_log.params_mapping))
+                .ok()
+        })
+        .collect();
+
+    println!("Decoded log: {:?}", out.clone().into_series());
+
+    Ok(Some(out.into_series()))
+}
+
+fn extract_log_fields(fields: &[Series]) -> PolarsResult<Vec<(Vec<FixedBytes<32>>, &[u8], &str)>> {
+    let zero_filled_topic = vec![0u8; 32];
+    
+    let fields_topic0 = fields[0].binary()?;
+    let fields_topic1 = fields[1].binary()?;
+    let fields_topic2 = fields[2].binary()?;
+    let fields_topic3 = fields[3].binary()?;
+    let fields_data = fields[4].binary()?;
+    let fields_sig = fields[5].str()?;
+
+    fields_topic0
+        .into_iter()
+        .zip(fields_topic1.into_iter())
+        .zip(fields_topic2.into_iter())
+        .zip(fields_topic3.into_iter())
+        .zip(fields_data.into_iter())
+        .zip(fields_sig.into_iter())
         .map(|(((((opt_topic0, opt_topic1), opt_topic2), opt_topic3), opt_data), opt_sig)| {
-            let zero_filled_topic = vec![0u8; 32];
             let topics = vec![
                 FixedBytes::from_slice(opt_topic0.unwrap_or(&zero_filled_topic)),
                 FixedBytes::from_slice(opt_topic1.unwrap_or(&zero_filled_topic)),
@@ -109,49 +133,61 @@ fn decode_log_batch(s: Series) -> PolarsResult<Option<Series>> {
             let data = opt_data.unwrap_or(&[]);
             let sig = opt_sig.unwrap_or("");
 
-            decode(sig, topics, data)
-                .map(|decoded_log| format!("{:?}, {:?}", decoded_log.indexed, decoded_log.body))
-                .ok()
+            Ok((topics, data, sig))
         })
-        .collect();
-
-    Ok(Some(out.into_series()))
+        .collect()
 }
 
-fn decode(full_signature: &str, topics: Vec<FixedBytes<32>>, data: &[u8]) -> Result<DecodedEvent, DecodeError> {
-    let event = Event::parse(full_signature)
-        .map_err(|e| DecodeError::DecodingError(e.to_string()))?;
-    let decoded_event = event.decode_log_parts(topics, data, false)
-        .map_err(|e| DecodeError::DecodingError(e.to_string()))?;
+fn decode(full_signature: &str, topics: Vec<FixedBytes<32>>, data: &[u8]) -> Result<ExtendedDecodedEvent, DecodeError> {
+    let event = parse_event_signature(full_signature)?;
+    let decoded_event = decode_event_log(&event, topics, data)?;
+    
+    let params = extract_event_params(&event, &decoded_event)?;
 
-    let mut params:Vec<Param> = vec![];
+    let params_keys_mapping: Vec<String> = params.iter().map(|p| p.name.clone()).collect();
+    let params_mapping = serde_json::to_string(&params).unwrap_or_else(|_| "[]".to_string());
+    
+    let extended_decoded_event = ExtendedDecodedEvent {
+        indexed: decoded_event.indexed,
+        body: decoded_event.body,
+        params_keys_mapping,
+        params_mapping
+    };
+    
+    Ok(extended_decoded_event)
+}
+
+fn parse_event_signature(full_signature: &str) -> Result<Event, DecodeError> {
+    Event::parse(full_signature)
+        .map_err(|e| DecodeError::DecodingError(e.to_string()))
+}
+
+fn decode_event_log(event: &Event, topics: Vec<FixedBytes<32>>, data: &[u8]) -> Result<DecodedEvent, DecodeError> {
+    event.decode_log_parts(topics, data, false)
+        .map_err(|e| DecodeError::DecodingError(e.to_string()))
+}
+
+fn extract_event_params(event: &Event, decoded_event: &DecodedEvent) -> Result<Vec<EventParam>, DecodeError> {
     let mut params_values: Vec<DynSolValue> = decoded_event.indexed.clone();
     params_values.extend(decoded_event.body.clone());
+
     if params_values.len() != event.inputs.len() {
-        return Err(DecodeError::DecodingError("Mismatch between signature lenght and retuned params lenght".to_string()));
+        return Err(DecodeError::DecodingError(
+            "Mismatch between signature length and returned params length".to_string()
+        ));
     }
-    if params_values.len() > 0 {
-        for (i, input) in event.inputs.iter().enumerate() {
-            let str_value = StringDynSolValue::from(params_values[i].clone());
-            let value = str_value.to_string_representation().unwrap_or("None".to_string());
-            let value_type = input.ty.to_string();
-            let name = input.name.clone();
-            let index = i as u32;
-            let p = Param {name, index, value_type, value};
-            params.push(p);
-        }
+
+    let mut params = Vec::new();
+    for (i, input) in event.inputs.iter().enumerate() {
+        let str_value = StringifiedValue::from(params_values[i].clone());
+        let param = EventParam {
+            name: input.name.clone(),
+            index: i as u32,
+            value_type: input.ty.to_string(),
+            value: str_value.convert().unwrap_or_else(|| "None".to_string()),
+        };
+        params.push(param);
     }
-    let params_keys_mapping: Vec<String> = params.iter().map(|p| p.name.clone()).collect();
-    println!("params_keys_mapping: {:?}", params_keys_mapping);
-    let params_mapping = parse_params_to_json(&params);
-    println!("Params JSON: {}", params_mapping);
-    
 
-    Ok(decoded_event)
-}
-
-fn parse_params_to_json(params: &Vec<Param>) -> String {
-    // Directly serialize the entire Vec of Param structs
-    serde_json::to_string(params)
-        .unwrap_or_else(|_| "[]".to_string())
+    Ok(params)
 }
