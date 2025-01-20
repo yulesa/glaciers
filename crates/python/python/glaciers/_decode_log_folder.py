a8d7cfcc882b763160581d958f99e025165ce69e
yulesa <yulesa@gmail.com>
2025-01-20 19:04:57 -0300

    adding support to pandas

--- crates/python/python/glaciers/_decode_log_folder.py
+++ crates/python/python/glaciers/_decode_log_folder.py
@@ -1,17 +1,18 @@
 import polars as pl
+from ._dataframe_utils import DataFrameType, to_prefered_type
 
 async def async_decode_log_folder(
     log_folder_path: str,
     abi_df_path: str,
-) -> pl.DataFrame:
+) -> DataFrameType:
     from . import _glaciers_python
     result: pl.DataFrame = await _glaciers_python.decode_log_folder(log_folder_path, abi_df_path)
-    return result
+    return to_prefered_type(result)
 
 def decode_log_folder(
     log_folder_path: str,
     abi_df_path: str,
-) -> pl.DataFrame:
+) -> DataFrameType:
 
     import asyncio
     coroutine = async_decode_log_folder(log_folder_path, abi_df_path)
@@ -22,8 +23,8 @@ def decode_log_folder(
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = executor.submit(loop.run_until_complete, coroutine)  # type: ignore
-            result: T = future.result()  # type: ignore
+            future = executor.submit(loop.run_until_complete, coroutine)
+            result = future.result()
     except RuntimeError:
         result = asyncio.run(coroutine)
 
