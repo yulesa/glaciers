851daddbc38be1fd4552fad997c90743c18094f1
yulesa <yulesa@gmail.com>
2025-02-05 11:19:46 -0300

    finishing trace_decoder

--- crates/glaciers/src/trace_decoder.rs
+++ crates/glaciers/src/trace_decoder.rs
@@ -1,5 +1,6 @@
 use alloy::dyn_abi::{DynSolValue, FunctionExt, JsonAbiExt};
 use alloy::json_abi::Function;
+use chrono::Local;
 use polars::prelude::*;
 use serde::Serialize;
 use std::fs;
@@ -46,14 +47,125 @@ struct ExtDecodedFunction {
     output_json: String,
 }
 
-pub async fn decode_trace_df(
-    trace_file_path: String,
+pub async fn decode_trace_folder(
+    trace_folder_path: String,
+    abi_df_path: String,
+) -> Result<(), TraceDecoderError> {
+
+    // Collect trace files' paths from trace_folder_path
+    let trace_files: Vec<PathBuf> = fs::read_dir(trace_folder_path)?
+        .filter_map(|entry| entry.ok())
+        .map(|entry| entry.path())
+        .collect();
+
+    // Create a semaphore with MAX_CONCURRENT_FILES_DECODING permits
+    let semaphore = Arc::new(Semaphore::new(get_config().trace_decoder.max_concurrent_files_decoding));
+    // Create a vector to hold our join handles
+    let mut handles = Vec::new();
+
+    // Spawn a task for each trace file
+    for trace_file_path in trace_files {
+        // skip PathBuf belonging to folders
+        if trace_file_path.is_dir() {
+            continue
+        }
+        // Clone the DataFrame and semafore for each task
+        let abi_df_path = abi_df_path.clone();
+        let semaphore = semaphore.clone();
+
+        // Spawn a tokio task for each file
+        let handle = task::spawn(async move {
+            // Acquire a permit before processing
+            let _permit = semaphore.acquire().await.unwrap();
+            decode_trace_file(trace_file_path, abi_df_path).await
+        });
+
+        handles.push(handle);
+    }
+
+    // Wait for all tasks to complete and collect errors
+    for handle in handles {
+        // Unwrap the outer Result from task::spawn
+        handle.await??;
+    }
+
+    println!(
+        "[{}] All files processed",
+        Local::now().format("%Y-%m-%d %H:%M:%S")
+    );
+    Ok(())
+}
+
+pub async fn decode_trace_file(
+    trace_file_path: PathBuf,
     abi_df_path: String,
 ) -> Result<DataFrame, TraceDecoderError> {
-    let trace_file_path = PathBuf::from(trace_file_path);
-    let abi_df_path = PathBuf::from(abi_df_path);
+    let file_path_str = trace_file_path.to_string_lossy().into_owned();
+    let file_name = trace_file_path
+        .file_name()
+        .unwrap()
+        .to_string_lossy()
+        .into_owned();
+    let mut file_folder_path = trace_file_path
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .to_string_lossy()
+        .into_owned();
 
-    let trace_df = utils::read_df_file(&trace_file_path)?;
+    println!(
+        "[{}] Starting decoding file: {}",
+        Local::now().format("%Y-%m-%d %H:%M:%S"),
+        file_path_str
+    );
+
+    let ethereum_traces_df = utils::read_df_file(&trace_file_path)?;
+    let ethereum_traces_df = utils::hex_string_columns_to_binary(ethereum_traces_df)?;
+    let mut decoded_df = decode_trace_df(ethereum_traces_df, abi_df_path).await?;
+
+    println!(
+        "[{}] Finished decoding file: {}",
+        Local::now().format("%Y-%m-%d %H:%M:%S"),
+        file_name
+    );
+    
+    if !file_folder_path.is_empty() {
+        file_folder_path = file_folder_path + "/";
+    }
+        
+    let save_path = format!(
+        "{}decoded/{}",
+        file_folder_path,
+        if file_name.contains("traces") {
+            file_name.replace("traces", "decoded_traces")
+        } else {
+            format!("decoded_traces_{}", file_name)
+        }
+    );
+
+    let save_path: &Path = Path::new(&save_path);
+
+    if let Some(parent) = save_path.parent() {
+        // create folder if it doesn't exist
+        fs::create_dir_all(parent.to_string_lossy().into_owned())?;
+    }
+    let save_path= save_path.with_extension(get_config().trace_decoder.output_file_format);
+    println!(
+        "[{}] Saving decoded traces to: {:?}",
+        Local::now().format("%Y-%m-%d %H:%M:%S"),
+        save_path
+    );
+    utils::write_df_file(&mut decoded_df, &save_path)?;
+
+    Ok(decoded_df)
+}
+
+pub async fn decode_trace_df (
+    trace_df: DataFrame,
+    abi_df_path: String,
+) -> Result<DataFrame, TraceDecoderError> {
+    let abi_df_path = Path::new(&abi_df_path);
     let abi_df = utils::read_df_file(&abi_df_path)?;
 
     decode_trace_df_with_abi_df(trace_df, abi_df).await
