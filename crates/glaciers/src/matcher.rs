63c9f7e9a55c7407cbb1e71c626262af139422db
yulesa <yulesa@gmail.com>
2025-02-06 14:54:50 -0300

    match_traces_by_4bytes

--- crates/glaciers/src/matcher.rs
+++ crates/glaciers/src/matcher.rs
@@ -92,3 +92,41 @@ pub fn match_traces_by_4bytes_address(trace_df: DataFrame, abi_df: DataFrame) ->
 
     Ok(traces_left_join_abi_df)
 }
+
+pub fn match_traces_by_4bytes(trace_df: DataFrame, abi_df: DataFrame) -> Result<DataFrame, MatcherError> {
+    let traces_1 = match_traces_by_4bytes_address(trace_df.clone(), abi_df.clone())?;
+    let trace_df_cols: Vec<Expr> = trace_df.get_columns().iter().map(|s| col(s.name())).collect();
+    let traces_address_matched = traces_1.clone().lazy().filter(col("full_signature").is_not_null()).collect()?;
+    let traces_address_not_matched = traces_1.lazy().filter(col("full_signature").is_null()).select(trace_df_cols);
+
+    // create an abi_df with the most frequent signature for each hash
+    let abi_df = abi_df
+        .lazy()
+        //count the number of rows for each full_signature
+        .group_by(["hash", "full_signature", "name"])
+        .agg([all().first(), len().alias("signature_count")])
+        //sort the rows by signature_count in descending order
+        .sort("signature_count", SortOptions {
+            descending: true,
+            nulls_last: true,
+            ..Default::default()}
+        )
+        // group by hash and num_indexed_args and keep the first row (most frequent hash and num_indexed_args)
+        .group_by(["hash"]).agg([
+            all().first()
+        ]).drop(["address", "signature_count"]);
+
+    let selector_alias = get_config().trace_decoder.trace_schema.trace_alias.selector;
+    let trace_2 = traces_address_not_matched
+            .join(
+                abi_df,
+                [col(selector_alias.as_str())],
+                [col("hash")],
+                JoinArgs::new(JoinType::Left),
+            )
+            .collect()?;
+
+    let traces_df = traces_address_matched.vstack(&trace_2)?;
+
+    Ok(traces_df)
+}
